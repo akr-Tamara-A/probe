9ac0de3fdceac798c11217a5ea5831d61822576f
Tamara A <akira-49-q@yandex.ru>
2020-06-10 23:05:06 +0300

    feat: site

---
+++ pages/index.js
@@ -0,0 +1,62 @@
+const popup = document.querySelector('.popup');
+const popupOpen = document.querySelector('.button__edit-profile');
+const popupClose = document.querySelector('.popup__close');
+
+function togglePopup() {
+  popup.classList.toggle('popup__opened');
+}
+
+popupOpen.addEventListener('click', togglePopup);
+popupClose.addEventListener('click', togglePopup);
+
+
+
+let formSubmit = document.querySelector('.popup__submit');
+
+function formSubmitHandler (evt) {
+  evt.preventDefault(); 
+  
+  let nameInput = document.querySelector('.popup__username')
+  let jobInput = document.querySelector('.popup__about')
+
+  let newUserName = nameInput.value;
+  let newUserJob = jobInput.value;
+
+  let userName = document.querySelector('.profile__user-name');
+  let userJob = document.querySelector('.profile__user-job');
+
+  console.log(nameInput, jobInput);
+  console.log(userName, userJob);
+  console.log(newUserName, newUserJob);
+
+  userName.textContent = newUserName;
+  userJob.textContent = newUserJob;
+}
+
+formSubmit.addEventListener('submit', formSubmitHandler);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
